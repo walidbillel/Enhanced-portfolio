e185db8a634e93532f9ac1339ea7bfafc19d3fd0
willou <walid.boubguira@gmail.com>
2020-07-15 16:53:39 -0400

    build

---
+++ client/build/precache-manifest.d5b944adc0f4a4dc7adf3ae62962016c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b51da31ec81751c1312ac13e81778c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c9be745e4780129edf",
+    "url": "/static/css/2.47fb9b64.chunk.css"
+  },
+  {
+    "revision": "769730acb75fbfc9d689",
+    "url": "/static/css/main.653be6fb.chunk.css"
+  },
+  {
+    "revision": "21c9be745e4780129edf",
+    "url": "/static/js/2.f6867465.chunk.js"
+  },
+  {
+    "revision": "769730acb75fbfc9d689",
+    "url": "/static/js/main.f852a9d0.chunk.js"
+  },
+  {
+    "revision": "fbe624b849e795bc7e9c",
+    "url": "/static/js/runtime-main.1c48cba3.js"
+  },
+  {
+    "revision": "b1a7a12c34ddc2dce288f15eb1ceaa6a",
+    "url": "/static/media/bg.b1a7a12c.jpg"
+  },
+  {
+    "revision": "40315fc427946adb191e2ff11ebea887",
+    "url": "/static/media/buisness_card.40315fc4.jpg"
+  },
+  {
+    "revision": "ce874aa290ab6ce53362c9891848a523",
+    "url": "/static/media/character-walid.ce874aa2.jpg"
+  },
+  {
+    "revision": "69500b4a6aeaf7a5e5d82930c292d19e",
+    "url": "/static/media/desert-iluustrated.69500b4a.jpg"
+  },
+  {
+    "revision": "3817dd738bb87b423953830ba8039689",
+    "url": "/static/media/logo.3817dd73.svg"
+  },
+  {
+    "revision": "8b75c1c50c6c32f231b5c7c5e410cf2e",
+    "url": "/static/media/moonstar.8b75c1c5.jpg"
+  },
+  {
+    "revision": "2b90735d49a2b3037ed94cc9d1a4b589",
+    "url": "/static/media/planet-walid.2b90735d.jpg"
+  },
+  {
+    "revision": "d99cb14a142d161cd56eb4dc6513a5c2",
+    "url": "/static/media/profile.d99cb14a.svg"
+  },
+  {
+    "revision": "c8950f77ef1574f4dbf07581eb7a1fc3",
+    "url": "/static/media/triangle.c8950f77.jpg"
+  },
+  {
+    "revision": "01f59bb67a591e91ac1d3e7102b6b8ef",
+    "url": "/static/media/wolf-moon.01f59bb6.jpg"
+  }
+]);
